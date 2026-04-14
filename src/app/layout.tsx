15984af6e660f00079569932f7e84b3a0fc1a5f6
Faizan Ali <f.ali@spctek.com>
2026-04-14 16:58:28 +0500

    feat: Integrate Google Tag Manager into the layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,30 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en" className="dark">
+			<head>
+				{/* Google Tag Manager */}
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MD27LC69');`,
+					}}
+				/>
+				{/* End Google Tag Manager */}
+			</head>
 			<body className="bg-[#020617] text-white antialiased">
+				{/* Google Tag Manager (noscript) */}
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-MD27LC69"
+						style={{ display: 'none', visibility: 'hidden' }}
+						height="0"
+						width="0"
+					/>
+				</noscript>
+				{/* End Google Tag Manager (noscript) */}
 				<ToastProvider />
 				{children}
 			</body>
